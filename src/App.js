a91b6bb0a650885220b9f9d72d9d331883aed9f4
Samuelys <samuelys@naver.com>
2023-03-25 13:39:25 +0900

    6-5 add code

--- src/App.js
+++ src/App.js
@@ -3,59 +3,34 @@ import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import {useRef, useState} from "react";
 
-// const diaryList = [
-//     {
-//         id: 1,
-//         author: "용성",
-//         content: "하이 1",
-//         emotion: 5,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 2,
-//         author: "영준",
-//         content: "안녕 2",
-//         emotion: 2,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 3,
-//         author: "정일",
-//         content: "잘자",
-//         emotion: 3,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 4,
-//         author: "영빈",
-//         content: "좋은아침",
-//         emotion: 4,
-//         created_date: new Date().getTime(),
-//     },
-// ];
-
 function App() {
     const [data, setData] = useState([]);
 
     const dataId = useRef(0);
 
     const onCreate = (author, content, emotion) => {
-      const created_date = new Date().getTime();
-      const newItem = {
-          author,
-          content,
-          emotion,
-          created_date,
-          id: dataId.current,
-      }
-      dataId.current+=1;
-      setData([newItem, ...data]);
+        const created_date = new Date().getTime();
+        const newItem = {
+            author,
+            content,
+            emotion,
+            created_date,
+            id: dataId.current,
+        }
+        dataId.current += 1;
+        setData([newItem, ...data]);
     };
+
+    const onDelete = (targetId) => {
+        console.log(targetId);
+        const newDiaryList = data.filter((it) => it.id !== targetId);
+        setData(newDiaryList);
+    }
 
     return (
         <div className="App">
-            <DiaryEditor onCreate={onCreate} />
-            <DiaryList diaryList={data}/>
+            <DiaryEditor onCreate={onCreate}/>
+            <DiaryList onDelete={onDelete} diaryList={data}/>
         </div>
     );
 }
