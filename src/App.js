5baa4159b60ea6cdd64c401c7c4deb8a19d37478
choify <samuelys@naver.com>
2023-03-29 15:33:16 +0900

    add code

--- src/App.js
+++ src/App.js
@@ -4,19 +4,47 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+import "./App.css";
 
 function App() {
+    const env = process.env;
     return (
         <BrowserRouter>
             <div className="App">
+                <MyHeader
+                    headText={"App"}
+                    leftChild={
+                        <MyButton text={"왼쪽 버튼"} onClick={()=>alert("왼쪽 클릭")} />
+                    }
+                    rightChild={
+                        <MyButton text={"오른쪽 버튼"} onClick={()=>alert("오른쪽 클릭")} />
+                    }
+                />
                 <h2>App.js</h2>
+                <MyButton
+                    text={"버튼"}
+                    onClick={() => alert("버튼 클릭")}
+                    type={"positive"}
+                />
+                <MyButton
+                    text={"버튼"}
+                    onClick={() => alert("버튼 클릭")}
+                    type={"negative"}
+                />
+                <MyButton
+                    text={"버튼"}
+                    onClick={() => alert("버튼 클릭")}
+                    type={"default"}
+                />
                 <Routes>
-                    <Route path={'/'} element={<Home />} />
-                    <Route path={'/new'} element={<New />} />
-                    <Route path={'/edit'} element={<Edit />} />
-                    <Route path={'/diary/:id'} element={<Diary />} />
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/new'} element={<New/>}/>
+                    <Route path={'/edit'} element={<Edit/>}/>
+                    <Route path={'/diary/:id'} element={<Diary/>}/>
                 </Routes>
-                <RouteTest />
+                <RouteTest/>
             </div>
         </BrowserRouter>
     );
